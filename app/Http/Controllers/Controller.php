b0db7b8921022d8174b2f3822271e7839c7d8da5
Matheus <D3rr3tido@gmail.com>
2025-01-02 15:16:54 -0300

    006

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,11 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function cleanUpperCaseString($string): string
+    {
+        // remover os espaçõs em branco no inicio e fim de uma string
+        // converter a string para uppercase
+
+        return strtoupper(trim($string));
+    }
 }
